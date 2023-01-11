c2dcf9d4a7af9f72a8eaa1bdd3b6ea341897fad6
Jhon <jhonro48@gmail.com>
2023-01-11 18:20:06 -0500

    Prueba 2

--- lib/user/ui/screens/profile_header.dart
+++ lib/user/ui/screens/profile_header.dart
@@ -1,42 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import '../../bloc/bloc_user.dart';
+import '../../model/user.dart';
 import '../witgets/user_info.dart';
 import '../witgets/button_bar.dart';
 
 class ProfileHeader extends StatelessWidget {
   late UserBlog userBloc;
+  late User user;
 
   Widget showProfileData(AsyncSnapshot snapshot) {
     if (!snapshot.hasData || snapshot.hasError) {
       print("No logueado");
+      return Container(
+        margin: EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                // title
+              ],
+            ),
+            CircularProgressIndicator(),
+            Text("No se pudo cargar la informacion. Haz login")
+          ],
+        ),
+      );
     } else {
-      print("logueado");
+      print(snapshot.data);
+      user = User(email: snapshot.data.email, name: snapshot.data.displayName, photoURL: snapshot.data.photoURL);
+      final title = Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0
+        ),
+      );
+
+      return Container(
+        margin: EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                title
+              ],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
     }
     return Container();
   }
 
   @override
   Widget build(BuildContext context) {
-
     userBloc = BlocProvider.of<UserBlog>(context);
 
     return StreamBuilder(
       stream: userBloc.streamFirebase,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        switch(snapshot.connectionState) {
+        switch (snapshot.connectionState) {
           case ConnectionState.waiting:
             return CircularProgressIndicator();
           case ConnectionState.none:
             return CircularProgressIndicator();
           case ConnectionState.active:
-            return Container();
+            return showProfileData(snapshot);
           case ConnectionState.done:
-            return Container();
+            return showProfileData(snapshot);
         }
       },
     );
-
 
 
     /*final title = Text(
@@ -67,5 +114,5 @@ class ProfileHeader extends StatelessWidget {
         ],
       ),
     );*/
-
+  }
 }
--- lib/user/ui/witgets/profile_place.dart
+++ lib/user/ui/witgets/profile_place.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plstzi_trips_app/place/model/place.dart';
 import 'profile_place_info.dart';
-import 'profile_place.dart';
 
 class ProfilePlace extends StatelessWidget {
 
